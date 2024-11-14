87702814cb20f159a5302bffb6fd72349c8ef2ac
Zhao Zhenye <522024230170@smail.nju.edu.cn>
2024-11-14 17:15:44 +0800

    add import student list function

--- src/views/ImportStudentView.vue
+++ src/views/ImportStudentView.vue
@@ -1,5 +1,13 @@
 <template>
 
+<el-upload action="/api/student/import"
+	      :show-file-list="false"
+	       :on-success="hanleSuccess"
+	       accept=".xls">
+	<el-button type="primary">导入名单</el-button>
+</el-upload>
+
+
 </template>
 
 <script>
